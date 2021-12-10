3da0b98fe0c884c80a9e07409eca28bb966705ee
tienpvse1 <tienpvse@fpt.edu.vn>
2021-12-10 14:35:00 +0700

    resolved(api): add api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './decorators/public.decorator';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('buoi')
+  @Public()
+  getBuoi() {
+    return 'buoi dai 20cm';
   }
 }
